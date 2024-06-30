38707ab6d052c073709888ca4bf681c2782243a5
Arachnid <89037932+ArachnidRB@users.noreply.github.com>
2024-06-30 11:35:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -178,8 +178,17 @@ function generateAd() {
         for (const id of ids) {
           receivingSide.push(parseFloat(id));
         }
-        let maxRId = receivingSide.reduce((maxId, id) => (itemValues[id].value > (itemValues[maxId].value || -Infinity) ? id : maxId), ids[0]);
-        let maxSId = sendingSide.reduce((maxId, id) => (itemValues[id].value > (itemValues[maxId].value || -Infinity) ? id : maxId), ids[0]);
+        let maxRId = receivingSide.reduce((maxId, id) => {
+          const currentValue = itemValues[`${id}`].value || -Infinity;
+          const maxValue = itemValues[`${maxId}`].value || -Infinity;
+          return currentValue > maxValue ? id : maxId;
+      }, ids[0]);
+
+        let maxSId = sendingSide.reduce((maxId, id) => {
+          const currentValue = itemValues[`${id}`].value || -Infinity;
+          const maxValue = itemValues[`${maxId}`].value || -Infinity;
+          return currentValue > maxValue ? id : maxId;
+      }, ids[0]);
         if (maxSId < maxRId) {
           receivingSide.push("upgrade");
         } else {
