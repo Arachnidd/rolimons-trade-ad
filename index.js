b36aa3a1fdecb49336650c294cfb60f81b801447
Arachnid <89037932+ArachnidRB@users.noreply.github.com>
2024-07-02 18:10:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ async function getInv() {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
     },
   }).then((res) => res.json()).then((json) => {
     playerInv = json.playerAssets; //gets the players inv
